8d8e1a479581adbf8e0cfa19085709d007beedc0
Lewuathe <lewuathe@me.com>
2020-12-24 07:48:53 +0900

    Use BuiltinTypes.h instead of StandardTypes.h
    StandardTypes.h was removed in https://reviews.llvm.org/D93430

--- include/Hello/HelloDialect.h
+++ include/Hello/HelloDialect.h
@@ -21,8 +21,8 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinDialect.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
-#include "mlir/IR/StandardTypes.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #include "Hello/HelloOpsDialect.h.inc"
