ee7a871ee57d350e7b0d3092d1bae104af31c4f6
Kai Sasaki <lewuathe@gmail.com>
2023-10-11 11:05:13 +0900

    Fix build failure

--- include/Hello/HelloOps.h
+++ include/Hello/HelloOps.h
@@ -18,10 +18,12 @@
 #ifndef HELLO_HELLOOPS_H
 #define HELLO_HELLOOPS_H
 
+#include "mlir/Bytecode/BytecodeOpInterface.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/Interfaces/FunctionInterfaces.h"
 
 #define GET_OP_CLASSES
 #include "Hello/HelloOps.h.inc"
